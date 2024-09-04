d9a50f4a3dd5bacb56e5a58d5a3c6e1a0584bd18
AdaChanona <ada.gamboa.chanona2510@gmail.com>
2024-09-04 00:36:44 -0600

    Inicio del servidor e instalación de librerias

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("hola mundo")
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+import { env } from "./env";
+
+const app = express();
+
+app.use(cors({
+    credentials: true,
+}))
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app)
+
+server.listen(env.PORT, ()=>{
+    console.log('Server running on http://'+env.HOST+':'+env.PORT+'/');
+});
